89a7c7e7baf5b0e9ef27e628a3089d681f2f2f09
luciolemos <lucio.lemos.385@ufrn.edu.br>
2023-09-18 20:45:39 -0300

    cOMMIT

---
+++ create_view.sql
@@ -0,0 +1,24 @@
+CREATE VIEW view_produtos_vendidos  
+AS SELECT produto_nome AS PRODUTO, 
+(SELECT SUM(total_unidades) FROM compras WHERE produto_codigo= codigo_produto)  
+AS QTDE FROM produtos;
+
+
+mysql> select*from view_produtos_vendidos;
++--------------------------+------+
+| PRODUTO                  | QTDE |
++--------------------------+------+
+| MACARRÃO                 |    2 |
+| SARDINHA                 |    4 |
+| KATCHUP                  |    6 |
+| MAIONEZE                 |    8 |
+| AZEITE                   |   10 |
+| PALMITO                  |    1 |
+| CARNE BOVINA             |    3 |
+| FRANGO CONGELADO         |    5 |
+| FILÉ DE PEIXE CONGELADO  |    7 |
+| CAFÉ                     |    9 |
+| LEITE LONG LIFE          |    9 |
+| AÇUCAR REFINADO          |   11 |
++--------------------------+------+
+12 rows in set (0.00 sec)
