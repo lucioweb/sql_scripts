171467d2ce8bfe21d58c2d9bef01a1c67e08e5cc
luciolemos <lucio.lemos.385@ufrn.edu.br>
2023-09-18 19:58:08 -0300

    Commit

---
+++ script.bash
@@ -0,0 +1,15 @@
+luciolemos@dev:~$ sudo mysql -u root -p
+Enter password:root
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 19
+Server version: 8.0.32-0ubuntu0.20.04.2 (Ubuntu)
+
+Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql>
---
+++ show_tables.bash
@@ -0,0 +1,12 @@
+mysql> show tables;
++--------------------------------------+
+| Tables_in_sistvendas                 |
++--------------------------------------+
+| clientes                             |
+| compras                              |
+| new_clientes                         |
+| produtos                             |
+| view_clientes_que_realizaram_compras |
+| view_produtos_vendidos               |
++--------------------------------------+
+6 rows in set (0.00 sec)
