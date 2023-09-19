9d6d707fd2f3e0112c844446efe75ae6f26fae52
luciolemos <lucio.lemos.385@ufrn.edu.br>
2023-09-19 07:37:37 -0300

    Commit

--- script.bash
+++ script.bash
@@ -13,3 +13,21 @@ owners.
 Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
 
 mysql>
+
+// Como usuário root
+
+luciolemos@dev:~$ sudo mysql
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 31
+Server version: 8.0.34-0ubuntu0.22.04.1 (Ubuntu)
+
+Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql>
+
